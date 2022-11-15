62d8d01918918c5f51b951c1b9994a3ea458ed9b
g00392495 <G00392495@atu.ie>
2022-11-15 13:29:29 +0000

    file instance where new file is created added

--- src/main/java/CreateFile.java
+++ src/main/java/CreateFile.java
@@ -1,11 +1,12 @@
 package ie.atu.week8;
 
 import java.io.*;
-
+import java.io.IOException;
 
 public class CreateFile {
-    public static void main(String[] args) {
-        File myFile = new File("MyFile.txt");
+    public static void main(String[] args) throws IOException {
+        File myFile = new File("PaddysFile.txt");
+        myFile.createNewFile();
         System.out.println("My file is located at: " + myFile.getAbsolutePath());
 
 
