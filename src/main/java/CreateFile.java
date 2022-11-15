8b82ecc5fb59a846dea3d1030ca3ad55c44b0f08
g00392495 <G00392495@atu.ie>
2022-11-15 14:11:34 +0000

    Used PrintWriter

--- src/main/java/CreateFile.java
+++ src/main/java/CreateFile.java
@@ -9,6 +9,15 @@ public class CreateFile {
         myFile.createNewFile();
         System.out.println("My file is located at: " + myFile.getAbsolutePath());
 
+        try {
+            PrintWriter outWriter = new PrintWriter(new FileWriter(myFile,true));
+            outWriter.write("My name is paddy");
+            outWriter.write("I am 19");
+            outWriter.close();
+        }
+        catch(IOException e){
+            System.out.println("Error occured");
+        }
 
     }
 }
